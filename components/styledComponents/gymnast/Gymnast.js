e040b14fb8554b4fe1c2335748639b617e1d9680
mudasir256 <smudasir256@gmail.com>
2023-08-01 19:29:34 +0500

    fix ui

--- components/styledComponents/gymnast/Gymnast.js
+++ components/styledComponents/gymnast/Gymnast.js
@@ -5,6 +5,25 @@ export const FormContainer = styled.div`
 
 `;
 
+export const Header = styled.div`
+display:flex;
+width:50%;
+background-color:gray;
+border-radius:10px;
+padding-left:25px;
+`;
+export const  LabelHeader = styled.div`
+color: #d5b93b;
+width:30%;
+`;
+export const  Wrapper = styled.div`
+color: #d5b93b;
+display:flex;
+justify-content: space-between;
+align-items:center;
+width:60%;
+`;
+
 export const AddChildWrapper = styled.div`
   display: flex;
   justify-content: center;
@@ -88,4 +107,45 @@ export const Select = styled.select`
   margin-left: 20px;
   width: 135px;
   background-color: #f8f8f8; /* Optional: Add background color to the dropdown */
+`;
+export const TableContainer = styled.div`
+color:#d5b93b;
+width:60%;
+margin:0px auto;
+margin-top:80px;
+`;
+export const TableWrapper = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  text-align: center;
+
+`;
+
+export const TableHead = styled.th`
+  padding: 10px;
+  background-color: #f2f2f2;
+  text-align: center;
+  border-radius: 5px;
+`;
+
+export const TableRow = styled.tr`
+  &:nth-child(even) {
+    background-color: #f2f2f2;
+    border-radius: 5px;
+    text-align: center;
+  }
+`;
+
+export const TableRow2 = styled.tr` {
+    background-color: #f2f2f2;
+    border-radius: 5px;
+    color:#d5b93b;
+  }
+`
+;
+export const TableCell = styled.td`
+  padding: 10px;
+  background-color: black;
+  color:white;
+  border:1px solid #d5b93b;
 `;
